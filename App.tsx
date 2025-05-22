b6834682d53053de95e5ed5ed4ae1b16cbfd0219
ChiXDD <danychi2003@gmail.com>
2025-05-22 10:41:26 -0600

    Archivo principal

--- App.tsx
+++ App.tsx
@@ -1,29 +1,38 @@
 import { HomeScreen } from './src/presentation/screens/HomeScreen'
-import APODScreen from './src/presentation/screens/APODScreen'
 import NeoWsScreen from './src/presentation/screens/NeoWsScreen'
 import EPICScreen from './src/presentation/screens/EPICScreen'
 import MarsRoverScreen from './src/presentation/screens/MarsRoverView'
 import LibraryScreen from './src/presentation/screens/LibraryScreen'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { HomeStackParamList } from './src/navigation/types'
 import { ROUTES } from './src/core/routes'
-import { ThemeProvider } from './src/core/ThemeContext'
+import { ThemeProvider, useTheme } from './src/core/ThemeContext'
+import { ThemeButton } from './src/core/ThemeButton'
 
-const Stack = createNativeStackNavigator<HomeStackParamList>()
+const Drawer = createDrawerNavigator<HomeStackParamList>()
+const Tab = createBottomTabNavigator<HomeStackParamList>()
+
+function EpicRoverTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false, tabBarIconStyle: { display: 'none' }, tabBarStyle: { backgroundColor: '#670D2F'}, tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'white', tabBarLabelStyle: { fontSize: 20 }, animation: 'shift' }}>
+      <Tab.Screen name={ROUTES.EPIC} component={EPICScreen} />
+      <Tab.Screen name={ROUTES.Rover} component={MarsRoverScreen} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <ThemeProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={ROUTES.Home}>
-          <Stack.Screen name={ROUTES.Home} component={HomeScreen} />
-          <Stack.Screen name={ROUTES.APOD} component={APODScreen} />
-          <Stack.Screen name={ROUTES.NeoWs} component={NeoWsScreen} />
-          <Stack.Screen name={ROUTES.EPIC} component={EPICScreen} />
-          <Stack.Screen name={ROUTES.Rover} component={MarsRoverScreen} />
-          <Stack.Screen name={ROUTES.Library} component={LibraryScreen} />
-        </Stack.Navigator>
+        <Drawer.Navigator initialRouteName={ROUTES.Home} screenOptions={{ headerStyle: { backgroundColor: '#670D2F' }, headerTintColor: 'white', drawerStyle: { backgroundColor: '#670D2F' }, drawerActiveBackgroundColor: '#31363F', headerTitleAlign: 'center', drawerLabelStyle: {color: 'white', fontSize: 17}, headerRight: () => <ThemeButton /> }}>
+          <Drawer.Screen name={ROUTES.Home} component={HomeScreen} />
+          <Drawer.Screen name={ROUTES['EPIC & Rover']} component={EpicRoverTabs} />
+          <Drawer.Screen name={ROUTES.NeoWs} component={NeoWsScreen} />
+          <Drawer.Screen name={ROUTES.Library} component={LibraryScreen} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </ThemeProvider>
   )
